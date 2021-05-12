66392ed330fcd8ad5621e8d9291bed5478eeefa2
Matteo Sangermani <matteo.sangermani@ntnu.no>
2021-05-12 14:03:31 +0200

    Finalized function optimise_PLS_CrossVal, improved pls_metrics

--- mbc_PLS_basic.py
+++ mbc_PLS_basic.py
@@ -9,11 +9,6 @@
 
 import numpy   as np
 import pandas  as pd
-import sklearn as skl
-import random
-import matplotlib.pyplot as plt
-
-from sys import stdout
 
 # ****************************************************************************
 
@@ -28,15 +23,18 @@ def PLS_ModelPredict( X_train, X_test, Y_train, Y_test, Ncomp):
     # The function creates a PLS model with a train data set. Then test it to
     # create prediction (Y_pred) that can be compared with Y_test to calculate
     # the error and accuracy
+    from sklearn.cross_decomposition import PLSRegression
+    from sklearn import metrics
+    from sklearn import metrics
 
-    plsr = skl.cross_decomposition.PLSRegression(n_components = Ncomp)
+    plsr = PLSRegression(n_components = Ncomp)
     plsr.fit( X_train, Y_train )
     Y_pred = plsr.predict( X_test )
     Y_pred = Y_pred.astype(float).flatten()
 
     # Calculate scores for MSE and R2
-    mse = skl.metrics.mean_squared_error( Y_test, Y_pred)
-    r2  = skl.metrics.r2_score( Y_test, Y_pred)
+    mse = metrics.mean_squared_error( Y_test, Y_pred)
+    r2  = metrics.r2_score( Y_test, Y_pred)
 
     return Y_pred, mse, r2
 
@@ -44,7 +42,8 @@ def PLS_ModelPredict( X_train, X_test, Y_train, Y_test, Ncomp):
 
 def optimise_PLS_CrossVal( M_X, M_Y, Test_Ncomp, uniqueID_Col,
                            RespVar, min_Resp_cat, max_Resp_cat, P_Threshold ,
-                           outLoop, inLoop, outerPropT2T, innerPropT2T):
+                           outLoop, inLoop, outerPropT2T, innerPropT2T,
+                           display_plot):
     # Perform a double cross validation of the PLR regression. The major steps
     # of the algorithm are the following:
     #
@@ -104,8 +103,7 @@ def optimise_PLS_CrossVal( M_X, M_Y, Test_Ncomp, uniqueID_Col,
                                                     iY_train, iY_test, cc)
                 innerMSE.iloc[iiL, cc-1 ] = mse
 
-            #print("Outer-loop: ", str(ooL+1), " ;    Inner-loop: ", str(iiL+1))
-            #plot_metrics( innerMSE.mean(axis=0).tolist() , 'MSE', 'min')
+            # --> Checkpoint Plot 1
 
         # Store average MSE for iiL-th inner loop, average on each modeled
         # number of components. Then, find the "leftmost" minimum MSE, which is
@@ -132,7 +130,15 @@ def optimise_PLS_CrossVal( M_X, M_Y, Test_Ncomp, uniqueID_Col,
         comparPred = pd.DataFrame( [oY_test.tolist(), oY_pred, Y_pred_thres, temp_accu] ).T
         comparPred.columns = ["oY_test", "oY_pred", "Y_pred_thres", "T-F"]
 
-    return accuracy, comparPred
+        # specificity is a ratio (N correct for class 1 / total number of class 1) - Sick
+        # selectivity is a ratio (N correct for class 2 / total number of class 2) - Healthy
+
+        # --> Checkpoint Plot 2
+        if display_plot:
+            print("Outer-loop: ", str(ooL+1))
+            plot_metrics( outerMSE.iloc[ooL, :].tolist() , 'MSE', 'min')
+
+    return accuracy, comparPred, outerMSE, innerMSE
 
 
 
@@ -140,22 +146,46 @@ def optimise_PLS_CrossVal( M_X, M_Y, Test_Ncomp, uniqueID_Col,
 def plot_metrics(vals, ylabel, objective):
     # Function to plot an np.array "vals" (either MSE or R2 from cross-valid.
     # of PLS) and display the min or max value with a cross
-    fig, ax = plt.subplots(figsize=(8, 3))
-    xticks = np.arange(1, len(vals)+1)
+    # vals      = can be a np.array or a single list. If it is np.array, each
+    #             column is an individual line to plot on sequential x positions
+    # ylabel    = (str) the name of the plotted y-values (e.g. MSE or R2)
+    # objective = (str) highlight minimum or maximum data point ("min" or "max")
 
-    plt.plot(xticks, np.array(vals), '-v', color='blue', mfc='blue')
+    import matplotlib.pyplot as plt
 
-    if objective=='min':
-        idx = np.argmin(vals)
+    if isinstance(vals, list):
+        x_Npoints = len(vals)
+        y_Nlines  = 1
     else:
-        idx = np.argmax(vals)
-    plt.plot(xticks[idx], np.array(vals)[idx], 'P', ms=10, mfc='red')
+        x_Npoints = vals.shape[0]
+        y_Nlines  = vals.shape[1]
 
-    ax.set_label('Number of PLS components in regression')
-    ax.set_ylabel(ylabel)
+    fig, ax = plt.subplots(figsize=(8, 3))
+    xticks = np.arange( 1 , x_Npoints+1)
+
+    # Plot all the line in the dataset vals
+    for ii in range(y_Nlines):
+        # Set the y_value to plot ii-th line
+        if isinstance(vals, list):
+            yValues = vals
+        else:
+            yValues = vals[:,ii]
+
+        plt.plot(xticks, np.array(yValues), '-v', color='blue', mfc='blue')
+
+        # Determine minimum and visualize it
+        if objective=='min':
+            idx = np.argmin(yValues)
+        else:
+            idx = np.argmax(yValues)
+        plt.plot(xticks[idx], np.array(yValues)[idx], 'P', ms=10, mfc='red')
+
+    # Adjust plot parameters
+    ax.set_xlabel('LVs in regression')
+    ax.set_ylabel( ylabel )
     ax.set_xticks( xticks )
     plt.grid(color = "grey", linestyle='--')
-    plt.title('PLS cross-val')
+    plt.title('Error in PLS models')
 
     plt.show()
 
@@ -173,7 +203,8 @@ def PLS_fit_model(M_X, M_Y, Ncomp, RespVar):
 
     # Define PLS object with Ncomp components. No scaling is necessary
     # (data was already scaled)
-    plsr = skl.cross_decomposition.PLSRegression(n_components= Ncomp , scale=False)
+    from sklearn.cross_decomposition import PLSRegression
+    plsr = PLSRegression(n_components= Ncomp , scale=False)
 
     # Build a PLS-DA model and save the values of opt_N_comp scores in DataFrame
     # together with the YY response table
@@ -218,7 +249,9 @@ def StandardScale_FeatureTable( M_X, idxCol ):
     # scale with mean = 0 and variance = 1    ( z=(x-u)/s )
     X_vars = M_X.iloc[ :, idxCol: ].copy()
     X_desc = M_X.iloc[ :, :idxCol ].copy()
-    temp_X = skl.preprocessing.StandardScaler().fit_transform( X_vars )
+
+    from sklearn.preprocessing import StandardScaler
+    temp_X = StandardScaler().fit_transform( X_vars )
 
     # Reformat into proper pd.DataFrame
     rows = X_vars.index.values
@@ -234,6 +267,7 @@ def RandomSelect_P_TrainTest( M_X, M_Y, uID_colname, proportion ):
     # Create training and test sets for M_X and M_Y, where all samples from one
     # patient are either training or test. Function assume that the two matrices
     # (M_X and M_Y) are same ordered list of observations
+    import random
 
     # Create a the unique list of identifiers to use to create the two sets
     uID_List  = np.unique( M_Y[ uID_colname ].values )
